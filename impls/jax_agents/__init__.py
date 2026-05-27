31190837be2db2b59a6082b1a2c65d529a8e6018
Catherine Glossop <catherine_glossop@berkeley.edu>
2026-05-27 12:52:53 -0700

    adding expo to agents

--- impls/jax_agents/__init__.py
+++ impls/jax_agents/__init__.py
@@ -6,14 +6,16 @@ from .gcivl import GCIVLAgent
 from .hiql import HIQLAgent
 from .qrl import QRLAgent
 from .sac import SACAgent
+from .expo import EXPOAgent
 
 agents = dict(
     crl=CRLAgent,
     dsrl=DSRLAgent,
     gcbc=GCBCAgent,
     gciql=GCIQLAgent,
-    gcivl=GCIVLAgent,
+    gcivl=GCIVLAgent,g
     hiql=HIQLAgent,
     qrl=QRLAgent,
     sac=SACAgent,
+    expo=EXPOAgent,
 )
